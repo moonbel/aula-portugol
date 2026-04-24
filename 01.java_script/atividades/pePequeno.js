cf7c49be5132b9eccb65bef2308177b171ea69d1
moonbel <hizabelesouza@gmail.com>
2026-04-23 22:07:27 -0300

    aula java

---
+++ 01.java_script/atividades/pePequeno.js
@@ -0,0 +1,12 @@
+// infos
+let preço, quantidade
+let valorTotal
+//leituras
+quantidade = Number(prompt("Quantidade"))
+preco = Number(prompt("Preço:"))
+valorTotal = quantidade * preco
+//proc
+
+//saidas
+console.log("Valor a receber: R$" + valorTotal)
+
---
+++ 01.java_script/atividades/testes.js
@@ -0,0 +1,41 @@
+//alert("Olá mundo!")
+//alert("SEGUNDA MENSAGEM")
+//let nome
+
+//let nome 
+
+//nome = "marco"
+//let idade = 18
+
+//alert("Idade: " + idade)
+//alert("Nome: " + nome + "\nidade: " + idade)
+
+//nome = prompt("Digite seu nome: ")
+//alert("Nome digitado: " + nome)
+
+//let nota1
+//notal = prompt("Digite a primeira nota: ")
+//notal = Number(nota1)
+//nota1 = nota1 + 1
+
+//aler(nota1)
+
+//============================================
+
+//informações
+
+let vitorias, empates
+let pontos
+
+//leituras 
+
+vitorias = prompt("vitórias:")
+vitorias = Number(vitorias)
+
+empates = Number(prompt("Empates: "))
+//processamentos
+pontos = vitorias*3 + empates
+//saidas
+
+alert("Total de ponto: " + pontos)
+//=============================================
