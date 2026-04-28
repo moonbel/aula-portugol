2fd4b061ab72d99a416b4fb77519935afdd89ef2
moonbel <hizabelesouza@gmail.com>
2026-04-28 19:17:27 -0300

    aula java

--- 01.java_script/atividades/manoJuca.js
+++ 01.java_script/atividades/manoJuca.js
@@ -1,4 +1,4 @@
-let moradia, agua, luz, internet, gasolina, salario, streamings, telefone, outros
+let moradia, agua, luz, internet, gasolina, salario, streamings, telefone
 let totalSoma
 salario = Number(prompt("Quanto você recebe de sálario: "))
 moradia = Number(prompt("Quanto gasta de moradia: "))
@@ -8,5 +8,5 @@ internet = Number(prompt("Quanto você gasta de internet: "))
 gasolina = Number(prompt("Quanto você gasta de gasolina: "))
 streamings =  Number(prompt("Quanto você gasta de streamings: "))
 telefone = Number(prompt("Quanto você gasta de telefone: "))
-totalSoma = salario- internet - gasolina - telefone - streamings - moradia - agua - luz
-console.log("O que te restou foi: ")
+totalSoma = (salario - internet - gasolina - telefone - streamings - moradia - agua - luz)
+console.log("O que te restou foi: " + totalSoma)
