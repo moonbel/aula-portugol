841de48af7509a6e8b511c15d71dc3362026e09d
moonbel <hizabelesouza@gmail.com>
2026-04-27 22:19:34 -0300

    organizacao

---
+++ 01.java_script/atividades/manoJuca.js
@@ -0,0 +1,12 @@
+let moradia, agua, luz, internet, gasolina, salario, streamings, telefone, outros
+let totalSoma
+salario = Number(prompt("Quanto você recebe de sálario: "))
+moradia = Number(prompt("Quanto gasta de moradia: "))
+agua = Number(prompt("Quanto você gasta de água: "))
+luz = Number(prompt("Quanto você gasta de luz: "))
+internet = Number(prompt("Quanto você gasta de internet: "))
+gasolina = Number(prompt("Quanto você gasta de gasolina: "))
+streamings =  Number(prompt("Quanto você gasta de streamings: "))
+telefone = Number(prompt("Quanto você gasta de telefone: "))
+totalSoma = salario- internet - gasolina - telefone - streamings - moradia - agua - luz
+console.log("O que te restou foi: ")
