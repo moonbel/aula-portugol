fa52dfba27b5d6a763eb0b50b91f80a69bc7fca3
moonbel <hizabelesouza@gmail.com>
2026-04-28 22:19:06 -0300

    aula java

---
+++ 01.java_script/atividades/gaelJares.js
@@ -0,0 +1,9 @@
+let caminhoes, lucroPorCaminhao, lucroTotal
+
+lucroPorCaminhao = (90 * 50) - 450
+//escreva(lucro por caminhão)
+ caminhoes= Number(prompt("Quantos caminhões: "))
+
+ lucroTotal = lucroPorCaminhao * caminhoes
+
+ console.log("Lucro da temporada: " + lucroTotal)
