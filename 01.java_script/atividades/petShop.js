63520b207c9b42bac80197d459e5fc496f504086
moonbel <hizabelesouza@gmail.com>
2026-04-28 21:33:37 -0300

    aula java

---
+++ 01.java_script/atividades/petShop.js
@@ -0,0 +1,9 @@
+let valorrama, valorFinal
+
+alert("Desconto de R$10,00 o kg na ração granel")
+
+valorrama = Number(prompt("Quantas gramas da ração: "))
+
+valorFinal = valorrama / 100*10
+
+console.log("o valor da ração é: R$" + valorFinal)
