f889ca4a32deed25a2475c8ec1c87a8219e19b62
moonbel <hizabelesouza@gmail.com>
2026-05-04 20:10:01 -0300

    aula java

--- 02 javascript/consumoCombustivel.js
+++
@@ -1,6 +0,0 @@
-let combustivelUsado, distanciaPercorrida, resultado
-combustivelUsado = Nmber(prompt("Quanto de combustível foi utilizado: "))
-distanciaPercorrida = Number(prompt("Quanto foi a distância percorrida: "))
-resultado = combustivelUsado / distanciaPercorrida
-
-console.log("A kilometragem por hora foi de: " + resultado)
