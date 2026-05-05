3a48fbc1c4c8039f8fdcf3fe8bba827de3b73a13
moonbel <hizabelesouza@gmail.com>
2026-05-05 20:01:10 -0300

    aula java

---
+++ 02.java_script/mediaPoderada2.js
@@ -0,0 +1,9 @@
+let notaA, notaB, notaTotal
+
+notaA = Number(prompt("Qual o valor da nota A: "))
+
+notaB = Number(prompt("Qual o valor da nota B: "))
+
+notaTotal = ((notaA * 3.5) + (notaB * 7.5)) / 11
+
+console.log("O valor total médio da soma das notas é: " + notaTotal)
---
+++ 02.java_script/mediaPoderada3.js
@@ -0,0 +1,5 @@
+let saldo, preco
+let saldoSuficiente, saldoNegativo
+saldo = Number(prompt("Qual seu saldo: "))
+
+preco = Number(prompt("Qual o preço do produto: "))
