89a2d5d958637747d8ee378e1b3a4f2ed1b6721a
moonbel <hizabelesouza@gmail.com>
2026-05-11 21:21:07 -0300

    aula java

--- 04.java_script/gaelJares.js
+++ 04.java_script/gaelJares.js
@@ -1,3 +1,22 @@
-let totalJares, caminhoes, quantidade
+let precoJares = 90.00, precoCaminhoes = 450.00, precoQuantidade = 50
+let quantidadeCaminhao, quantidadeJare
+let somaJare, somaCaminhao, somaQuantidade, total
 
-caminhoes = Number(prompt("Quantos caminhões foram vendidos nessa temporada: "))
+quantidadeCaminhao = Number(prompt("Quantos caminhões estão sendo transportados: "))
+
+quantidadeJare = Number(prompt("Quantos jarés estão sendo levado em cada caminhão: "))
+
+somaCaminhao = (precoCaminhoes * quantidadeCaminhao)
+
+somaJare = (precoJares * quantidadeJare)
+
+console.log("O faturamento de caminhões que você teve hoje foi de: R$" + somaCaminhao.toFixed(2))
+
+console.log("\nO custo que você teve com os jarés foi de: " + somaJare.toFixed(2))
+
+total = (somaCaminhao + somaJare)
+console.log("\nO lucro que você teve no otal foi de: " + total.toFixed(2))
+
+
+
+
