dae56cd03bae0d0c24f2e92a3de9df5af6cb9f7e
moonbel <hizabelesouza@gmail.com>
2026-05-12 21:37:37 -0300

    operadores

---
+++ 04.java_script/capitaoGanso.js
@@ -0,0 +1,18 @@
+let resposta = document.getElementById('resultado')
+let lucroObtido, lucroPercentual, gastoSuprimento
+let suprimentos, ingressos, gastos, lucro
+
+suprimentos = Number(prompt("Quanto foi gasto em suprimentos: "))
+
+ingressos = Number(prompt("Quanto foi gasto em ingressos: "))
+
+lucro=  ()
+gastos = (suprimentos + ingressos)
+if(suprimentos > ingressos){
+    mensagem = "Você faturou mais com os suprimentos"
+}else{
+    (suprimentos < ingressos)
+    mensagem = "Você faturou mais com os ingressos"
+} resposta.innerHTML = mensagem
+
+console.log("Os gastos que vocÊ")
---
+++ 04.java_script/tellesTransportes.js
@@ -0,0 +1,3 @@
+let resposta = document.getElementById('resultado')
+let mensagem
+let presoBruto, total, soma, pesoLiquido
