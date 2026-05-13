d54ba0d7978d6879185166c2f66b1470f0ff52db
moonbel <hizabelesouza@gmail.com>
2026-05-13 19:41:23 -0300

    aula java

--- 04.java_script/tellesTransportes.js
+++ 04.java_script/tellesTransportes.js
@@ -1,3 +1,8 @@
-let resposta = document.getElementById('resultado')
-let mensagem
-let presoBruto, total, soma, pesoLiquido
+let frete, distancia, volume, peso
+
+peso = Number(prompt("Qual é o peso: "))
+distancia = Number(prompt("Qual é a distância: "))
+volume = Number(prompt("Qual é o volume: "))
+
+frete = 15 + (2*peso)+(0.05*distancia)+(10*volume)
+console.log("O valor do frete vai ser: " + frete)
