5e7bf1d7e20d57b357661769b68f2dfe73861f59
moonbel <hizabelesouza@gmail.com>
2026-07-07 22:14:20 -0300

    arrays

--- 09.arrays/array.js
+++ 09.arrays/array.js
@@ -1,32 +1,143 @@
 let dias = ['segunda ', 'terça ', 'quarta ', 'quinta ', 'sexta ', 'sábado ', 'domingo '];
-let numerosPares = [2, 4, 12, 8, 10, 12, 14, 16, 18, 20]
+let numerosPares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+let frutas = ['banana', 'maçã', 'uva']
+let amigos = ['luanna', 'erick', 'diego']
+let idades = ['17', '19', '16', '20', '21', '24']
+let alimento = ['Maçãs', 'Pão', 'Leite']
+
 
 function diasdaSemana() {
     for (let i = 0; i < dias.length; i++) {
 
-document.getElementById("resultado").innerHTML += " "
-document.getElementById("resultado").innerHTML += dias[i]
- }
+        document.getElementById("resultado").innerHTML += " "
+        document.getElementById("resultado").innerHTML += dias[i]
+    }
 }
 
 function adicionarFeriado() {
-document.getElementById("resultado").innerHTML = " "
+    document.getElementById("resultado").innerHTML = " "
 
-dias.push('halloween')
-diasdaSemana()
+    dias.push('halloween')
+    diasdaSemana()
 }
 
 function removerUltimoDia() {
-document.getElementById("resultado").innerHTML = " "
+    document.getElementById("resultado").innerHTML = " "
 
-dias.pop()
-diasdaSemana()
+    dias.pop()
+    diasdaSemana()
 }
 
-function numerosPare(){
-    for (let i = 2; i < numerosPares.length; i++){
+function numerosPare() {
+    document.getElementById("resultado").innerHTML = " "
 
-document.getElementById("resultado").innerHTML += " "
-document.getElementById("resultado").innerHTML += numerosPares[i]
- }
+    for (let i = 0; i < numerosPares.length; i++) {
+
+        document.getElementById("resultado").innerHTML += " "
+        document.getElementById("resultado").innerHTML += numerosPares[i]
+    }
+}
+
+function substituirNumeroCeis() {
+    document.getElementById("resultado").innerHTML = " "
+    numerosPares.splice(2, 1, "12");
+    numerosPare()
+}
+function frutasSequencia() {
+    for (let i = 0; i < frutas.length; i++) {
+
+        document.getElementById("resultado").innerHTML += " "
+        document.getElementById("resultado").innerHTML += frutas[i]
+
+    }
+}
+
+function morango() {
+    document.getElementById("resultado").innerHTML = " "
+    frutas.push('morango')
+    frutasSequencia()
+
+}
+function removeMaca() {
+    document.getElementById("resultado").innerHTML = " "
+    frutas.splice(1, 1,);
+    frutasSequencia()
+}
+function nomesAmigos() {
+    for (let i = 0; i < amigos.length; i++) {
+        document.getElementById("resultado").innerHTML += " "
+        document.getElementById("resultado").innerHTML += amigos[i]
+    }
+}
+function maisAmigos() {
+    document.getElementById("resultado").innerHTML = " "
+    amigos.push('athos', 'mariom')
+    nomesAmigos()
+}
+
+function idadeAmigos() {
+    for (let i = 0; i < idades.length; i++) {
+        document.getElementById("resultado").innerHTML += " "
+        document.getElementById("resultado").innerHTML += idades[i]
+    }
+}
+
+function somaIdades() {
+    let soma = Number(idades[0]) + Number(idades[1]);
+    document.getElementById("resultado").innerHTML = soma;
+}
+function media() {
+    let soma = 0;
+
+    for (let i = 0; i < idades.length; i++) {
+        soma += Number(idades[i]);
+    }
+    let media = soma / idades.length;
+    document.getElementById("resultado").innerHTML = media;
+}
+function carrinhoCompra() {
+        document.getElementById("resultado").innerHTML = " "
+    
+    for (let i = 0; i < alimento.length; i++) {
+        document.getElementById("resultado").innerHTML += " "
+        document.getElementById("resultado").innerHTML += alimento[i] 
+    }
+}
+function adicioneMaisComida() {
+
+    document.getElementById("resultado").innerHTML += " "
+
+    alimento.push('ovos', 'arroz')
+
+    carrinhoCompra();
+}
+
+function removerTerceiro() {
+    document.getElementById("resultado").innerHTML = " "
+    alimento.splice(2, 1);
+    carrinhoCompra()
+}
+function verificarPao() {
+    document.getElementById("resultado").innerHTML = alimento.includes('Pão');
+}
+
+function adicionarImpares() {
+    numeros = [];
+
+    for (let i = 1; i <= 9; i += 2) {
+        numeros.push(i);
+    }
+
+    document.getElementById("resultado").innerHTML = numeros;
+}
+
+
+function multiplicarPorDois() {
+
+
+    for (let i = 0; i < numeros.length; i++) {
+        resultado.push(numeros[i] * 2);
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
 }
